201b75cf6f7a5561d2d92747ad08e9da2b918794
bisbilge <bisbilge@gmail.com>
2026-03-23 19:10:28 +0000

    fix: inceleme bölümü kaldırıldı, sadece yorum sistemi bırakıldı

--- wikiboxd/users/urls.py
+++ wikiboxd/users/urls.py
@@ -6,32 +6,27 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('ara/', views.user_search, name='user_search'),
-    path('<str:username>/', views.user_profile, name='user_profile'),
-    path('<str:username>/takip/', views.toggle_follow, name='toggle_follow'),
-    
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    
-    # Şifremi Unuttum Rotaları (Güncellendi)
     path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='users/password_reset.html',
-        email_template_name='users/password_reset_email.html', # E-posta şablonunu belirttik
-        success_url=reverse_lazy('users:password_reset_done')  # Başarılı olunca buraya git
+        email_template_name='users/password_reset_email.html',
+        success_url=reverse_lazy('users:password_reset_done')
     ), name='password_reset'),
-    
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='users/password_reset_done.html'
     ), name='password_reset_done'),
-    
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='users/password_reset_confirm.html',
-        success_url=reverse_lazy('users:password_reset_complete') # Başarılı olunca buraya git
+        success_url=reverse_lazy('users:password_reset_complete')
     ), name='password_reset_confirm'),
-    
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='users/password_reset_complete.html'
     ), name='password_reset_complete'),
+    # Dinamik pattern'ler en sona
+    path('<str:username>/', views.user_profile, name='user_profile'),
+    path('<str:username>/takip/', views.toggle_follow, name='toggle_follow'),
 ]
